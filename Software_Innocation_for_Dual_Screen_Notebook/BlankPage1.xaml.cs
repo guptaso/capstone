42a0b40741ea3100155e25fb418e1f0db3039bd8
guptaso <Colder#22so>
2020-02-08 15:09:58 -0800

    buttonnn

--- Software_Innocation_for_Dual_Screen_Notebook/BlankPage1.xaml.cs
+++ Software_Innocation_for_Dual_Screen_Notebook/BlankPage1.xaml.cs
@@ -25,6 +25,44 @@ namespace Software_Innocation_for_Dual_Screen_Notebook
         public BlankPage1()
         {
             this.InitializeComponent();
+
+            this.confirm_btn.Click += ConfirmClick;
+            this.cancel_btn.Click += CancelClick;
+
         }
+
+        private void CancelClick(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage),"");
+        }
+
+        private void ConfirmClick(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.nameInput.Text.ToString()))
+            {
+                this.Frame.Navigate(typeof(MainPage), "error");
+            }
+            else
+            {
+                Dictionary<string, string> newDictionary = new Dictionary<string, string>();
+                newDictionary.Add("name", this.nameInput.Text);
+                newDictionary.Add("image", this.pngInput.Text);
+                
+                if (string.IsNullOrEmpty(this.appInput.Text.ToString()))
+                {
+                    newDictionary.Add("keyboard", this.keyboardInput.Text);
+                    newDictionary.Add("app", "");
+                    this.Frame.Navigate(typeof(MainPage), newDictionary);
+                }
+                else
+                {
+                    newDictionary.Add("app", this.appInput.Text);
+                    newDictionary.Add("keyboard", "");
+                    this.Frame.Navigate(typeof(MainPage), newDictionary);
+                }
+                
+            }
+        }
+    
     }
 }
--- Software_Innocation_for_Dual_Screen_Notebook/MainPage.xaml.cs
+++ Software_Innocation_for_Dual_Screen_Notebook/MainPage.xaml.cs
@@ -5,12 +5,14 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -27,9 +29,113 @@ namespace Software_Innocation_for_Dual_Screen_Notebook
             this.InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        public int row = 1;
+        public int column = 1;
+        private void Click_Cancel(Object sender, RoutedEventArgs e)
         {
-           this.Frame.Navigate(typeof(BlankPage1));
+            /* Emptys out the textboxes */
+            this.nameInput.Text = "";
+            this.appInput.Text = "";
+            this.keyboardInput.Text = "";
+            this.pngInput.Text = "";
+
+            //this.Frame.Navigate(typeof(MainPage), "");
+
+        }
+        private async void btnLaunchMap_Click(object sender, RoutedEventArgs e)
+            
+        {
+            String[] spearator = { ","};
+            String[] strlist = appInput.Text.Split(spearator, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (String s in strlist)
+            {
+                Uri uri = new Uri(s);
+                await Launcher.LaunchUriAsync(uri);
+
+            }
+
+            //Uri uri = new Uri("bingmaps:?rtp=adr.Washington,%20DC~adr.New%20York,% 20NY & amp; mode = d & amp; trfc = 1");
+            //Uri uri = new Uri("spotify:");
+           // Uri uri = new Uri(appInput.Text);
+           // await Launcher.LaunchUriAsync(uri);
+
+        }
+
+
+        private async void Click_Confirm(Object sender, RoutedEventArgs e)
+        {
+            /* adds generic elements to a button */
+            Button newButton = new Button();
+            newButton.Content = "no shortcut name";
+            newButton.Width = btn_add.Width;
+            newButton.Height = btn_add.Height;
+            newButton.CornerRadius = btn_add.CornerRadius;
+            newButton.Opacity = btn_add.Opacity;
+            newButton.Background = btn_add.Background;
+            newButton.BorderBrush = btn_add.BorderBrush;
+            //newButton.FontSize = btn_add.FontSize;
+            newButton.FontSize = 30;
+            newButton.Padding = btn_add.Padding;
+            newButton.Margin = btn_add.Margin;
+            /* adds the input information */
+            newButton.Content = this.nameInput.Text;
+            //newButton.KeyDown = "";
+            
+            
+
+           if (!String.IsNullOrEmpty(appInput.Text))
+            {
+                //var success = await Windows.System.Launcher.LaunchUriAsync(new Uri(@appInput.Text));
+                //newButton.Click += btnLaunchMap_Click;
+                newButton.Click += async (s, en) => {
+                    String[] spearator = { "," };
+                    String[] strlist = appInput.Text.Split(spearator, StringSplitOptions.RemoveEmptyEntries);
+
+                    foreach (String st in strlist)
+                    {
+                        Uri uri = new Uri(st);
+                        await Launcher.LaunchUriAsync(uri);
+                        //System.Diagnostics.Process.Start(st);
+                    }
+                };
+            }
+            myGrid.Children.Add(newButton);
+            myFlyout.Hide();
+
+
+            /* Emptys out the textboxes *//*
+            this.nameInput.Text = "";
+            this.appInput.Text = "";
+            this.keyboardInput.Text = "";
+            this.pngInput.Text = "";*/
+
+        }
+
+        void sendUrl(string s)
+        {
+            //launchURI_Click(s);
+        }
+        private async void launchURI_Click(object sender, EventArgs e)
+        {
+            // The URI to launch
+            var uriBing = new Uri(@"http://www.bing.com");
+
+            // Launch the URI
+            var success = await Windows.System.Launcher.LaunchUriAsync(uriBing);
+
+            if (success)
+            {
+                // URI launched
+            }
+            else
+            {
+                // URI launch failed
+            }
+        }
+
+        private void btn_add_Click(object sender, RoutedEventArgs e)
+        {
 
         }
     }
