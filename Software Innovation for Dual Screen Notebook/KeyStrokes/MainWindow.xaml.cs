4c1192306bb6cf9e6ca0d112791b6b9fc021ce78
Bao Phung <phungb@oregonstate.edu>
2020-02-13 20:38:14 -0800

    In MainWindow.xaml.cs, change the absolute path.

---
+++ Software Innovation for Dual Screen Notebook/KeyStrokes/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace KeyStrokes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        //Keys register on window.
+        private void KeyInteractor(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.D1) {
+                MessageBox.Show("The \'1\' key was pressed, opening Legends of Runeterra", "Button 1 Press");
+                e.Handled = true; //prevent the action from happening twice.
+                Button1_KeyDown(sender, e);
+            }
+            else if(e.Key == Key.D2) {
+                MessageBox.Show("The \'2\' key was pressed, opening r/leagueoflegends", "Button 2 Press");
+                e.Handled = true; //prevent the action from happening twice.
+                Button2_KeyDown(sender, e);
+            }
+            else if(e.Key == Key.D3) {
+                MessageBox.Show("The \'3\' key was pressed, opening YouTube", "Button 3 Press");
+                e.Handled = true; //prevent the action from happening twice.
+                Button3_KeyDown(sender, e);
+            }
+            else if(e.Key == Key.OemPlus)
+            {
+                MessageBox.Show("The \'+\' key was pressed, time to create a new button", "Button + Press");
+                e.Handled = true; //prevent the action from happening twice.
+                Add_KeyDown(sender, e);
+            }
+        }
+
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button was clicked, time to create a new button", "Button + Click");
+            Form1 createButton = new Form1();
+            createButton.Show();
+        }
+
+        private void Add_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.OemPlus)
+            {
+                Form1 createButton = new Form1();
+                createButton.Show();
+            }
+        }
+
+        
+
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button was clicked, opening Legends of Runeterra", "Button 1 Click");
+            Process.Start("D:\\Riot Games\\LoR\\live\\Game\\LoR");
+        }
+
+
+        private void Button1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.D1)
+                Process.Start("D:\\Riot Games\\LoR\\live\\Game\\LoR");
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button was clicked, opening League of Legends Subreddit", "Button 2 Click");
+            Process.Start("https://www.reddit.com/r/leagueoflegends");
+        }
+
+        private void Button2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.D2)
+                Process.Start("https://www.youtube.com");
+        }
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button was clicked, opening YouTube", "Button 1 Click");
+            Process.Start("D:\\Riot Games\\LoR\\live\\Game\\LoR");
+        }
+
+
+        private void Button3_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.D3)
+                Process.Start("https://www.youtube.com");
+        }
+    }
+}
