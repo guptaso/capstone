273e8965b708ad23f5ef50c4b79de62320948781
guptaso <Colder#22so>
2020-03-02 23:34:35 -0800

    move buttons, max height/width for main buttons

--- Software Innovation for Dual Screen Notebook/KeyStrokes/ButtonGridControl.xaml.cs
+++ Software Innovation for Dual Screen Notebook/KeyStrokes/ButtonGridControl.xaml.cs
@@ -18,7 +18,7 @@ namespace KeyStrokes
         private List<Button> buttonList;
         private int col = 0;
         private int row = 0;
-
+        private Button hold;
         public ButtonGridControl()
         {
             InitializeComponent();
@@ -32,32 +32,148 @@ namespace KeyStrokes
         {
             var template = (ControlTemplate)buttonGrid.FindResource("button");
             Button b = new Button { Template = template };
-            
+
             b.Content = content;
             b.Width = Double.NaN;
+            b.MaxHeight = 50;
+            b.MaxWidth = 100;
 
             // set the click handler
             b.Click += click.Invoke;
-            
+
             // option to remove the button
             //newButton.RightTapped += async (s, en) =>
             b.MouseDown += async (s, en) =>
             {
-                MessageBoxResult result = MessageBox.Show("Remove?", "", MessageBoxButton.YesNo);
+
+                /* MessageBoxResult result = MessageBox.Show("Remove?", "", MessageBoxButton.YesRemoveNo);
                 switch (result)
                 {
                     case MessageBoxResult.Yes:
+
+                        // remove button
                         grid.Children.Remove(b);
+                        buttonList.Clear();
+                        foreach(Button buttonItem in grid.Children)
+                        {
+                            if (buttonItem is FrameworkElement)
+                            {
+                                buttonList.Add(buttonItem);
+                            }
+                        }
+                        
                         break;
                 }
+                */
+                btnMenu.Visibility = Visibility.Visible;
+                hold = b;
+
+            };
+
+            b.MouseDoubleClick += async (s, en) =>
+            {
+                btnMenu.Visibility = Visibility.Visible;
+                hold = b;
             };
 
 
             // change this to actaully set the col and row
             //Grid.SetColumn(b, col++);
             //Grid.SetRow(b, row);
+
+            // add button to the list
             buttonList.Add(b);
-            grid.Children.Add(b);
+
+            //grid.Children.Add(b);
+
+            // re-adds the buttons from the list 
+            grid.Children.Clear();
+            foreach (Button buttonItem in buttonList)
+            {
+                grid.Children.Add(buttonItem);
+            }
+        }
+
+        public void removeBtn(object sender, RoutedEventArgs e)
+        {
+            // remove button
+            grid.Children.Remove(hold);
+            buttonList.Clear();
+            foreach (Button buttonItem in grid.Children)
+            {
+                if (buttonItem is FrameworkElement)
+                {
+                    buttonList.Add(buttonItem);
+                }
+            }
+
+            btnMenu.Visibility = Visibility.Hidden;
+        }
+
+        public void okBtn(object sender, RoutedEventArgs e)
+        {
+            rName.Visibility = Visibility.Visible;
+            rowIn.Visibility = Visibility.Visible;
+            cName.Visibility = Visibility.Visible;
+            colIn.Visibility = Visibility.Visible;
+            but.Visibility = Visibility.Visible;
+        }
+
+        public void changeBtn(object sender, RoutedEventArgs e)
+        {
+            int newRow = Int32.Parse(rowIn.Text);
+            int newCol = Int32.Parse(colIn.Text);
+
+            // get new index 
+            // (newRow * number of cols set currently) + newRow = new index 
+            int curRows = grid.Rows;
+            int curCols = grid.Columns;
+            //int a = ((curRows-1)*newRow) + (newCol);
+            int a = ((newRow - 1) * curCols + newCol);
+
+            // remove button
+            grid.Children.Remove(hold);
+            buttonList.Clear();
+            foreach (Button buttonItem in grid.Children)
+            {
+                if (buttonItem is FrameworkElement)
+                {
+                    buttonList.Add(buttonItem);
+                }
+            }
+
+            if ((a) <= buttonList.Count + 1)
+            {
+                // insert button at new position
+                Console.WriteLine("count: ");
+                Console.WriteLine(buttonList.Count);
+                buttonList.Insert(a - 1, hold);
+            }
+            else
+            {
+                buttonList.Add(hold);
+            }
+            // clear grid
+            grid.Children.Clear();
+
+            // re-format grid
+            foreach (Button buttonItem in buttonList)
+            {
+                grid.Children.Add(buttonItem);
+            }
+
+
+
+            // clear and close everything
+            btnMenu.Visibility = Visibility.Hidden;
+            rowIn.Text = "";
+            colIn.Text = "";
+            rName.Visibility = Visibility.Hidden;
+            rowIn.Visibility = Visibility.Hidden;
+            cName.Visibility = Visibility.Hidden;
+            colIn.Visibility = Visibility.Hidden;
+            but.Visibility = Visibility.Hidden;
+
         }
 
         // this opens the new window for adding new buttons
@@ -71,28 +187,10 @@ namespace KeyStrokes
 
         public void set_grid(int row, int col)
         {
-                grid.Rows = row;
-                grid.Columns = col;
+            grid.Rows = row;
+            grid.Columns = col;
         }
 
-        private void Button_KeyDown(object sender, KeyEventArgs e)
-        {
 
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
 }
