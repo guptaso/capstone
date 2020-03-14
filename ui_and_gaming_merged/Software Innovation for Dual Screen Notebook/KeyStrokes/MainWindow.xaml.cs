e222b312ece6b320793627d194805968793bb56b
Bao Phung <baophungus@gmail.com>
2020-03-14 08:49:06 -0700

    Finished merging ui and gaming branches

---
+++ ui_and_gaming_merged/Software Innovation for Dual Screen Notebook/KeyStrokes/MainWindow.xaml.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+using System.Collections.Generic;
+using System.Linq;
+using MaterialDesignThemes.Wpf;
+
+namespace KeyStrokes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        private const int WM_MOUSEACTIVATE = 0x0021;
+        private const int MA_NOACTIVATE = 3;
+        private const int WS_EX_NOACTIVE = 0x08000000;
+        private const int GWL_EXSTYLE = -20;
+
+        // this is used to get the hWnd for imported functions
+        // hWnd is a way to identify windows used in win32 framework
+        private WindowInteropHelper helper;
+        private List<VirtualKeyShort.Key> shortcut;
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll")]
+        public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hwnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            shortcut = new List<VirtualKeyShort.Key>();
+
+            // sets the window so that a click does not bring it into focus
+            helper = new WindowInteropHelper(this);
+            SetWindowLong(helper.Handle, GWL_EXSTYLE, GetWindowLong(helper.Handle, GWL_EXSTYLE) | WS_EX_NOACTIVE);
+
+            // this lets WndProc be overriden so that we can get the click massage
+            HwndSource source = PresentationSource.FromVisual(this) as HwndSource;
+            source.AddHook(WndProc);
+
+            // this activates the window so that when we start it 
+            // it does not jump to the back of all windows
+            this.Activate();
+        }
+
+        // this gets the click message so that 
+        // it can still sends the click to the app
+        // even though it is out of focus
+        IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            // Handle messages...
+            switch (msg)
+            {
+                case WM_MOUSEACTIVATE:
+                    return (IntPtr)MA_NOACTIVATE;
+                default:
+                    break;
+            }
+
+            return IntPtr.Zero;
+        }
+
+
+        //Keys register on window.
+        private void KeyInteractor(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.D1)
+            {
+                MessageBox.Show("The \'1\' key was pressed, opening Legends of Runeterra", "Button 1 Press");
+                e.Handled = true; //prevent the action from happening twice.
+                Button1_KeyDown(sender, e);
+            }
+            else if (e.Key == Key.D2)
+            {
+                MessageBox.Show("The \'2\' key was pressed, opening r/leagueoflegends", "Button 2 Press");
+                e.Handled = true; //prevent the action from happening twice.
+                Button2_KeyDown(sender, e);
+            }
+            else if (e.Key == Key.D3)
+            {
+                MessageBox.Show("The \'3\' key was pressed, opening YouTube", "Button 3 Press");
+                e.Handled = true; //prevent the action from happening twice.
+                Button3_KeyDown(sender, e);
+            }
+            else if (e.Key == Key.OemPlus)
+            {
+                MessageBox.Show("The \'+\' key was pressed, time to create a new button", "Button + Press");
+                e.Handled = true; //prevent the action from happening twice.
+                Add_KeyDown(sender, e);
+            }
+        }
+
+
+        // this opens the new window for adding new buttons
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            //AddButtonWindow addButton = new AddButtonWindow();
+            //addButton.InitializeComponent();
+            ////addButton.Show();
+        }
+
+
+        private void Add_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.OemPlus)
+            {
+                Form1 createButton = new Form1();
+                createButton.Show();
+            }
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button was clicked, opening Legends of Runeterra", "Button 1 Click");
+            Process.Start("D:\\Riot Games\\LoR\\live\\Game\\LoR");
+        }
+
+
+        private void Button1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.D1)
+                Process.Start("D:\\Riot Games\\LoR\\live\\Game\\LoR");
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button was clicked, opening League of Legends Subreddit", "Button 2 Click");
+            Process.Start("https://www.reddit.com/r/leagueoflegends");
+        }
+
+        private void Button2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.D2)
+                Process.Start("https://www.youtube.com");
+        }
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button was clicked, opening YouTube", "Button 1 Click");
+            Process.Start("D:\\Riot Games\\LoR\\live\\Game\\LoR");
+        }
+
+
+        private void Button3_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.D3)
+                Process.Start("https://www.youtube.com");
+        }
+
+
+        // Bottom bar, all the words should be changed to icons and made to look much much better
+        private void media_back_Click(object sender, RoutedEventArgs e)
+        {
+            Shortcut.send(new VirtualKeyShort.Key[] { VirtualKeyShort.Key.MEDIA_PREV_TRACK });
+        }
+
+        private void media_play_pause_Click(object sender, RoutedEventArgs e)
+        {
+            // this needs to be changed to accomidate actually checking if the 
+            // music is playing to be correct all the time
+            if (media_play_pause.Content.Equals("Play"))
+            {
+                media_play_pause.Content = new PackIcon { Kind = PackIconKind.PlayPause };
+            }
+            else
+            {
+                media_play_pause.Content = new PackIcon { Kind = PackIconKind.PlayPause };
+            }
+            Shortcut.send(new VirtualKeyShort.Key[] { VirtualKeyShort.Key.MEDIA_PLAY_PAUSE });
+        }
+
+        private void media_forward_Click(object sender, RoutedEventArgs e)
+        {
+            Shortcut.send(new VirtualKeyShort.Key[] { VirtualKeyShort.Key.MEDIA_NEXT_TRACK });
+        }
+
+        private void undo_Click(object sender, RoutedEventArgs e)
+        {
+            Shortcut.send(new VirtualKeyShort.Key[] { VirtualKeyShort.Key.CONTROL, VirtualKeyShort.Key.KEY_Z });
+        }
+
+        private void redo_Click(object sender, RoutedEventArgs e)
+        {
+            Shortcut.send(new VirtualKeyShort.Key[] { VirtualKeyShort.Key.CONTROL, VirtualKeyShort.Key.KEY_Y });
+        }
+
+        private void grid_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ui_and_gaming_merged/Software Innovation for Dual Screen Notebook/KeyStrokes/TitleBar.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+using System.Collections.Generic;
+
+namespace KeyStrokes
+{
+    public partial class TitleBar : UserControl
+    {
+        private MainWindow main;
+
+        public TitleBar()
+        {
+            InitializeComponent();
+            main = ((MainWindow)App.Current.MainWindow);
+        }
+
+        private void titlebar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            main.Activate();
+            main.DragMove();
+        }
+
+
+        private void close_Click(object sender, RoutedEventArgs e)
+        {
+            main.Close();
+        }
+
+        private void maximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (main.WindowState == WindowState.Normal)
+            {
+                main.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                main.WindowState = WindowState.Normal;
+            }
+        }
+
+        private void minimize_Click(object sender, RoutedEventArgs e)
+        {
+            main.WindowState = WindowState.Minimized;
+        }
+
+        private void MenuClick(object sender, RoutedEventArgs e)
+        {
+            if (main.menu_control.IsVisible)
+            {
+                main.menu_control.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                main.Activate();
+                main.menu_control.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
